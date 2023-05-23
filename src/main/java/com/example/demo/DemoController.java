d05875debda2809696e5af71672fdda8a56514a6
Mitsu <adieuer._.03.26@icloud.com>
2023-05-23 17:37:45 +0900

    First

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DemoController {
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "お名前を入力してください");
+		return mav;
+	}
+
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(
+			@RequestParam("text1")String str,
+			ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "こんにちは" + str + "さん！");
+		mav.addObject("value", str);
+		return mav;
+	}
+}
